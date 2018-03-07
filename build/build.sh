e3f3b5242295648016e6de26e6416c5bc65b1374
Alexander Kravchenko <mr.aleksandr.kravchenko@gmail.com>
2018-03-07 14:10:37 +0300

    scritps polishing

--- build/build.sh
+++ build/build.sh
@@ -14,6 +14,9 @@ rm -fR $BUILD_DIR
 rm -fR $REDIST_DIR
 mkdir -p $BUILD_DIR
 mkdir -p $REDIST_DIR
+mkdir -p $REDIST_DIR/bin
+mkdir -p $REDIST_DIR/include
+mkdir -p $REDIST_DIR/lib
 
 cd $BUILD_DIR
 
@@ -22,14 +25,14 @@ then
 	export CC='cl.exe'
 	# this option can be used in single thread compilation (make withot -jX option)
 	# --extra-cflags="-Zi"
-	${SOURCE_DIR}/configure --enable-shared --host=$HOST_DEF --cross-prefix=$CROSS_PREFIX
+	${SOURCE_DIR}/configure --enable-shared --host=$HOST_DEF --cross-prefix=$CROSS_PREFIX --prefix="./../../../x264_redist/$1"
 else
-	${SOURCE_DIR}/configure --enable-shared --host=$HOST_DEF --cross-prefix=$CROSS_PREFIX --sysroot=$CROSSBUILDTREE --extra-ldflags=-static-libgcc
+	${SOURCE_DIR}/configure --enable-shared --host=$HOST_DEF --cross-prefix=$CROSS_PREFIX --sysroot=$CROSSBUILDTREE --extra-ldflags=-static-libgcc --prefix="./../../../x264_redist/$1"
 fi
 
 make -j8
 
-if [ "$COMPILER" = "msvc" ]; then
+if [ "$COMPILER" == "msvc" ]; then
    . "${TOOLS_DIR}/make_def_and_lib_from_dll.sh"
    make_def_and_lib_from_dll libx264-155.dll libx264.lib
 fi
@@ -38,17 +41,17 @@ cd $ROOT_DIR
 
 cp -v $BUILD_DIR/x264.pc     $REDIST_DIR
 
-cp -v $SOURCE_DIR/x264.h          $REDIST_DIR
-cp -v $BUILD_DIR/x264_config.h    $REDIST_DIR
-cp -v $BUILD_DIR/*.dll            $REDIST_DIR
-#cp -v $BUILD_DIR/*.exe            $REDIST_DIR
+cp -v $SOURCE_DIR/x264.h          $REDIST_DIR/include
+cp -v $BUILD_DIR/x264_config.h    $REDIST_DIR/include
+cp -v $BUILD_DIR/*.dll            $REDIST_DIR/bin
+cp -v $BUILD_DIR/*.exe            $REDIST_DIR/bin
 
 if [ "$COMPILER" == "msvc" ]
 then
-    cp -v $BUILD_DIR/libx264.dll.lib $REDIST_DIR/x264.lib
-    cp -v $BUILD_DIR/libx264.dll.lib $REDIST_DIR/libx264.lib
+    cp -v $BUILD_DIR/libx264.dll.lib $REDIST_DIR/lib/x264.lib
+    cp -v $BUILD_DIR/libx264.dll.lib $REDIST_DIR/lib/libx264.lib
 else
-    cp -v $BUILD_DIR/libx264.dll.a $REDIST_DIR/x264.a
-    cp -v $BUILD_DIR/libx264.dll.a $REDIST_DIR/libx264.a
+    cp -v $BUILD_DIR/libx264.dll.a $REDIST_DIR/lib/x264.a
+    cp -v $BUILD_DIR/libx264.dll.a $REDIST_DIR/lib/libx264.a
 fi
 
